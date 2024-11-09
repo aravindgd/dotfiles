b59dedbb6914ddf1ece9a81aec44d249dfd3e97c
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2024-11-09 10:40:36 +0100

    Do codefolding with treesitter

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -50,6 +50,8 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- spell check
 vim.api.nvim_create_autocmd('FileType', {
