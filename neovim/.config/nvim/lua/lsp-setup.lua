31a6fb0fb635d89bebb2b4bde20d4d86812c99cc
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2024-11-09 10:43:09 +0100

    Remove ruby_lsp neovim version related conditional code
    neovim supports textDocument/diagnostic now so removing the conditional
    code to make it work

--- neovim/.config/nvim/lua/lsp-setup.lua
+++ neovim/.config/nvim/lua/lsp-setup.lua
@@ -1,50 +1,35 @@
 -- local lsp_config = require('lspconfig')
 -- [[ Configure LSP ]]
 
--- ONLY FOR ruby_ls
--- textDocument/diagnostic support until 0.10.0 is released
-local _timers = {}
-local function setup_diagnostics_for_ruby_ls(client, buffer)
-  if require("vim.lsp.diagnostic")._enable then
-    return
-  end
+-- ONLY FOR ruby_lsp
+local function add_ruby_deps_command(client, bufnr)
+  vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps", function(opts)
+    local params = vim.lsp.util.make_text_document_params()
+    local showAll = opts.args == "all"
 
-  local diagnostic_handler = function()
-    local params = vim.lsp.util.make_text_document_params(buffer)
-    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-      if err then
-        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
-        vim.lsp.log.error(err_msg)
+    client.request("rubyLsp/workspace/dependencies", params, function(error, result)
+      if error then
+        print("Error showing deps: " .. error)
+        return
       end
-      local diagnostic_items = {}
-      if result then
-        diagnostic_items = result.items
-      end
-      vim.lsp.diagnostic.on_publish_diagnostics(
-        nil,
-        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
-        { client_id = client.id }
-      )
-    end)
-  end
 
-  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+      local qf_list = {}
+      for _, item in ipairs(result) do
+        if showAll or item.dependency then
+          table.insert(qf_list, {
+            text = string.format("%s (%s) - %s", item.name, item.version, item.dependency),
+            filename = item.path
+          })
+        end
+      end
 
-  vim.api.nvim_buf_attach(buffer, false, {
-    on_lines = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
-    end,
-    on_detach = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-    end,
-  })
+      vim.fn.setqflist(qf_list)
+      vim.cmd('copen')
+    end, bufnr)
+  end,
+  {nargs = "?", complete = function() return {"all"} end})
 end
--- ONLY FOR ruby_ls
+-- ONLY FOR ruby_lsp
 
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(client, bufnr)
@@ -60,6 +45,10 @@ local on_attach = function(client, bufnr)
     end
 
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  if client.name == 'ruby_lsp' then
+    add_ruby_deps_command(client, bufnr)
   end
 
   nmap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame buffer')
