29ef41f26d93b77eb0a7730c121b11084d42e817
Aravind Gopalakrishnan <pro.aravind@gmail.com>
2025-05-19 14:55:40 +0200

    simple rename to find plugin easily

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ return {
 	-- Fuzzy Finder (files, lsp, etc)
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       -- fuzzy finder algorithm
